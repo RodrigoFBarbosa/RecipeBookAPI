7ba06bfcd2bb98be269c1c5c56def171ca5aabb9
Rodrigo <rodrigobarbosa.p99@gmail.com>
2025-05-21 15:03:11 -0300

    Reapply "test: add unit and integration tests to do login"
    This reverts commit 33498b0825648805b7cad8e40b9df4be8eb04037.

--- tests/WebApi.Test/CustomWebApplicationFactory.cs
+++ tests/WebApi.Test/CustomWebApplicationFactory.cs
@@ -3,11 +3,16 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using MyRecipeBook.Infrastructure.DataAccess;
 using Microsoft.Extensions.DependencyInjection;
+using CommonTestUtilities.Entities;
+using MyRecipeBook.Domain.Entities;
 
 namespace WebApi.Test;
 
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
+    private MyRecipeBook.Domain.Entities.User _user = default!;
+    private string _password = string.Empty;
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Test")
@@ -16,7 +21,7 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
                 var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<MyRecipeBookDbContext>));
                 if (descriptor is not null)
                     services.Remove(descriptor);
-
+                 
                 var provider = services.AddEntityFrameworkInMemoryDatabase().BuildServiceProvider();
 
                 services.AddDbContext<MyRecipeBookDbContext>(options =>
@@ -24,6 +29,28 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
                     options.UseInMemoryDatabase("InMemoryDbForTesting");
                     options.UseInternalServiceProvider(provider);
                 });
+
+                using var scope = services.BuildServiceProvider().CreateScope();
+
+                var dbContext = scope.ServiceProvider.GetRequiredService<MyRecipeBookDbContext>();
+
+                dbContext.Database.EnsureDeleted();
+
+                StartDatabase(dbContext);
+                
             });
+    }
+
+    public string GetEmail() => _user.Email;
+    public string GetPassword() => _password;
+    public string GetName() => _user.Name;
+
+    private void StartDatabase(MyRecipeBookDbContext dbContext)
+    {
+        (_user, _password) = UserBuilder.Build();
+
+        dbContext.Users.Add(_user);
+
+        dbContext.SaveChanges();
     }
 }
