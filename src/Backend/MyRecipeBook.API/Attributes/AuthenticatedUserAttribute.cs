6af6eb82cff0b28c92d888eeb96cb4ce7b8a5ce5
Rodrigo <rodrigobarbosa.p99@gmail.com>
2025-06-13 11:58:19 -0300

    fix SonarCloud metrics

--- src/Backend/MyRecipeBook.API/Attributes/AuthenticatedUserAttribute.cs
+++ src/Backend/MyRecipeBook.API/Attributes/AuthenticatedUserAttribute.cs
@@ -3,6 +3,7 @@ using MyRecipeBook.API.Filters;
 
 namespace MyRecipeBook.API.Attributes;
 
+
 public class AuthenticatedUserAttribute : TypeFilterAttribute
 {
     public AuthenticatedUserAttribute() : base(typeof(AuthenticatedUserFilter))
